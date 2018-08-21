785ca606555c9d1f8b3a67910bc37a4c5e1c19a5
Shivam Chopra <shivamdev31@gmail.com>
2018-08-21 18:48:50 +0530

    Add app module files.

---
+++ app/src/main/java/io/kotlin/kotlinclean/feed/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.kotlin.kotlinclean.feed
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.kotlin.kotlinclean.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
